1158d6b8cc4fe3a8152182508aaf58c1b269ff64
ravindu629 <ravinduwijerathne629@gmail.com>
2022-08-14 15:28:48 +0530

    created backend file structure, server.js file and installed npm packages

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once("open", () => {
+  console.log("MongoDB Connected");
+});
+
+app.get("/", (req, res) => {
+  res.send("Api running");
+});
+
+app.use("/api/admins", require("./routes/Admin.route"));
+app.use("/api/staffMembers", require("./routes/Staff.route"));
+app.use("/api/students", require("./routes/Student.route"));
+app.use("/api/markingSchemas", require("./routes/MarkingSchema.route"));
+app.use("/api/submissions", require("./routes/Submission.route"));
+
+app.use("/api/addMarks", require("./routes/SupervisorMarks.route"));
+app.use("/api/acceptTopic", require("./routes/AcceptTopic.route"));
+
+app.use("/api/studentGroups", require("./routes/StudentGroup.route"));
+app.use("/api/researchTopics", require("./routes/ResearchTopic.route"));
+app.use("/api/stdDocfiles", require("./routes/StdDocFiles.route"));
+
+app.use("/api/PanelMember", require("./routes/PanelMemberMarks.route"));
+
+app.use("/api/PanelMember", require("./routes/PanelMemberMarks.route"));
+
+app.use("/api/files", require("./routes/Files.route"));
+
+app.listen(port, () => {
+  console.log("Server is starting on port " + port);
+});
