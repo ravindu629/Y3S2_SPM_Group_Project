57fb0b97ba51e44fd58fe22358b76d41ad36fa7b
ravindu629 <ravinduwijerathne629@gmail.com>
2022-08-31 23:46:29 +0530

    created addmin home page

--- client/src/components/admin/AdminMenu.js
+++ client/src/components/admin/AdminMenu.js
@@ -1,5 +1,30 @@
 import React from "react";
+import "./admin.css";
 
 export default function AdminMenu() {
-  return <div>AdminMenu</div>;
+  return (
+    <div className="adminMenu">
+      <div className="heading">Admin Menu</div>
+      <div className="admBtn">
+        <button type="button" className="btn btn-light btn-lg btn-block">
+          <div className="adminMenuBtns">{">"} Manage Staff Members</div>
+        </button>
+
+        <button type="button" className="btn btn-light btn-lg btn-block">
+          <div className="adminMenuBtns">{">"} Manage Students</div>
+        </button>
+
+        <button type="button" className="btn btn-light btn-lg btn-block">
+          <div className="adminMenuBtns">{">"} Manage Admins</div>
+        </button>
+
+        <button type="button" className="btn btn-light btn-lg btn-block">
+          <div className="adminMenuBtns">{">"} Register New Staff Member</div>
+        </button>
+        <button type="button" className="btn btn-light btn-lg btn-block">
+          <div className="adminMenuBtns"> {">"} Register New Admin</div>
+        </button>
+      </div>
+    </div>
+  );
 }
